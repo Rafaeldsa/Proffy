f902715cfd9467d25c64d089ce597ce58c91f936
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2020-08-03 11:47:19 -0300

    begin

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
+function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+          </div>
+        </form>
+      </PageHeader>
+    </div>
+  );
+}
+
+export default TeacherList;
